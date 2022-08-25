42bfdde37cb9cf6ebe0cb4c2cc2163614b0fc5a9
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-08-24 18:09:40 -0600

    firebase firestore setup

---
+++ my-money/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firebase-firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEJegvLBrEAmu19MDlrYCZXbSNmdH6-rE",
+  authDomain: "udemy-netninja-financetracker.firebaseapp.com",
+  projectId: "udemy-netninja-financetracker",
+  storageBucket: "udemy-netninja-financetracker.appspot.com",
+  messagingSenderId: "852228709496",
+  appId: "1:852228709496:web:f9b450e74112102526a18d"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
